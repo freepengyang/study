2e025a5672349679024079f10b55766ba33fd711
freepengyang <pengyang19951004@outlook.com>
2018-06-01 11:08:28 +0000

    Delete ipvsadm.sh

--- ipvsadm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sed -i '/^IPVS_MODULES_UNLOAD/s/yes/no/' /etc/sysconfig/ipvsadm-config
- sed -i '/^IPVS_SAVE_ON_STOP/s/no/yes/' /etc/sysconfig/ipvsadm-config
-sed -i '/^IPVS_SAVE_ON_RESTART/s/no/yes/' /etc/sysconfig/ipvsadm-config
-systemc	restart ipvsadm
-ipvsadm -S > /etc/sysconfig/ipvsadm
-systemc	enable ipvsadm
